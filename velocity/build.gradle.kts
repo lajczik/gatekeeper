c902716a37067737e9cf00819d728199f3d17285
lajczi <lajczi@gmail.com>
2026-05-08 20:19:27 +0200

    Removed unused annotation processor.

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     compileOnly(project(":shared"))
 
     compileOnly("com.velocitypowered:velocity-api:3.5.0-SNAPSHOT")
-    annotationProcessor("com.velocitypowered:velocity-api:3.5.0-SNAPSHOT")
+    //annotationProcessor("com.velocitypowered:velocity-api:3.5.0-SNAPSHOT") plugin doesn't need this
 }
 
 tasks {
