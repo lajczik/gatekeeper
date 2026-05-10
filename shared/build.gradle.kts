9941c8f04075f7f0ab46cd86030dd968b3f778d2
lajczi <lajczi@gmail.com>
2026-05-10 23:43:13 +0200

    Removed unused dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,18 +3,8 @@ plugins {
     id("com.gradleup.shadow")
 }
 
-dependencies {
-    implementation("com.zaxxer:HikariCP:7.0.2")
-    implementation("com.h2database:h2:2.4.240")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.5.8")
-}
-
 tasks {
     shadowJar {
-        minimize {
-            exclude(dependency("com.zaxxer:HikariCP"))
-            exclude(dependency("com.h2database:h2"))
-            exclude(dependency("org.mariadb.jdbc:mariadb-java-client"))
-        }
+        minimize()
     }
 }
